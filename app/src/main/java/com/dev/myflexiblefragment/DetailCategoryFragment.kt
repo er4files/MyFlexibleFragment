6e465cc49203b2d10bbcd51684643fdf4667e1e8
er4files <rahmaderasugiarto@gmail.com>
2024-03-04 15:08:51 +0700

    Initial commit

---
+++ app/src/main/java/com/dev/myflexiblefragment/DetailCategoryFragment.kt
@@ -0,0 +1,66 @@
+package com.dev.myflexiblefragment
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+
+class DetailCategoryFragment : Fragment() {
+
+    private lateinit var tvCategoryName: TextView
+    private lateinit var tvCategoryDescription: TextView
+    private lateinit var btnProfile: Button
+    private lateinit var btnShowDialog: Button
+    var description: String? = null
+    var optionDialogListener: OptionDialogFragment.OnOptionDialogListener? = null
+
+    companion object {
+        var EXTRA_NAME = "extra_name"
+        var EXTRA_DESCRIPTION = "extra_description"
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_detail_category, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        tvCategoryName = view.findViewById(R.id.tv_category_name)
+        tvCategoryDescription = view.findViewById(R.id.tv_category_description)
+        btnProfile = view.findViewById(R.id.btn_profile)
+        btnShowDialog = view.findViewById(R.id.btn_show_dialog)
+
+        btnShowDialog.setOnClickListener{
+            val optionDialogFragment = OptionDialogFragment()
+            optionDialogFragment.show(childFragmentManager, OptionDialogFragment::class.java.simpleName)
+        }
+        btnProfile.setOnClickListener{
+            val intent = Intent(requireActivity(), ProfileActivity::class.java)
+            startActivity(intent)
+        }
+
+        if (savedInstanceState != null) {
+            val descFromBundle = savedInstanceState.getString(EXTRA_DESCRIPTION)
+            description = descFromBundle
+        }
+
+        if (arguments != null) {
+            val categoryName = arguments?.getString(EXTRA_NAME)
+            tvCategoryName.text = categoryName
+            tvCategoryDescription.text = description
+        }
+
+        // Initialize optionDialogListener
+        optionDialogListener = object : OptionDialogFragment.OnOptionDialogListener {
+            override fun onOptionChosen(text: String?) {
+                Toast.makeText(requireContext(), text, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
